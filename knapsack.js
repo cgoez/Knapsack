99b5c86953278d76b2593950da0fdebdd1ff95f2
cgoez <goez2001@gmail.com>
2018-06-07 16:02:46 -0700

    memoizedKnapsack solution, pull commit

--- knapsack.js
+++ knapsack.js
@@ -2,7 +2,8 @@ const fs = require("fs");
 
 /* 
 =====  Naive Recursive Approach =====
-=====  "Brute force" method =====
+  "Brute force" method
+  Super ineffective
 */
 function naiveKnapsack(items, capacity) {
   function recurse(i, size) {
@@ -11,11 +12,13 @@ function naiveKnapsack(items, capacity) {
       return {
         value: 0,
         size: 0,
-        chosen: []
+        chosen: [],
+        msg: "naiveKnapsack"
       };
     }
 
     // Check to see if item fits
+    // if no, move on to next item
     else if (items[i].size > size) {
       return recurse(i - 1, size);
     }
@@ -38,6 +41,76 @@ function naiveKnapsack(items, capacity) {
   return recurse(items.length - 1, capacity);
 }
 
+/* 
+=====  Memoized Recursive Approach =====
+  Save results in cache to reduce runtime
+  Faster!
+*/
+function memoizedKnapsack(items, capacity) {
+  //Initialize cache (matrix, 2d array)
+  const cache = Array(items.length);
+
+  //Add second dimension
+  for (let i = 0; i < items.length; i++) {
+    cache[i] = Array(capacity + 1).fill(null);
+  }
+
+  function recurseMemo(i, size) {
+    let value = cache[i][size];
+
+    if (!value) {
+      value = recurseNaive(i, size);
+      cache[i][size] = value;
+    }
+
+    return value;
+  }
+
+  function recursiveNaive(i, size) {
+    // Base case
+    if (i === -1) {
+      return {
+        value: 0,
+        size: 0,
+        chosen: [],
+        msg: "memoizedKnapsack"
+      };
+    }
+
+    // Check to see if item fits
+    // if no, move on to next item
+    else if (items[i].size > size) {
+      return recurseMemo(i - 1, size);
+    }
+    // Item fits, but might not be worth as much as items in knapsack already
+    else {
+      const r0 = recurseMemo(i - 1, size);
+      const r1 = recurseMemo(i - 1, size - items[i].size);
+
+      r1.value += items[i].value;
+
+      if (r0.value > r1.value) {
+        return r0;
+      } else {
+        r1.size += items[i].size;
+        r1.chosen = r1.chosen.concat(i + 1);
+        return r1;
+      }
+    }
+  }
+
+// ===== Fail memoizing =====
+//   function knapsackMemo(i, size) {
+//     let value = cache[i][size];
+//   }
+//   function recurse(i, size) {
+//     if (!cache) {
+//       return cache;
+//     }
+//   }
+  return recurseMemo(items.length - 1, capacity);
+}
+
 /*
 =====  Greedy Strategy =====
   0. Go through our items and filter out any items whose size > knapsack's capacity
@@ -51,7 +124,8 @@ const greedyAlgo = (items, capacity) => {
   const result = {
     size: 0,
     value: 0,
-    chosen: []
+    chosen: [],
+    msg: "greedyAlgo"
   };
 
   //   items = items.filter(item => item.size < capacity);
@@ -63,14 +137,14 @@ const greedyAlgo = (items, capacity) => {
     return r2 - r1;
   });
 
-  // Loop through out items array, checking to see if the
+  // Loop through our items array, checking to see if the
   // item's size <= our total capacity
   for (let i = 0; i < items.length; i++) {
     if (items[i].size <= capacity) {
       // if it is, add it to our final result
       result.size += items[i].size;
-      results.value += items[i].value;
-      results.chosen.push(items[i].index);
+      result.value += items[i].value;
+      result.chosen.push(items[i].index);
       // dont forget to decrement our total capacity
       capacity -= items[i].size;
     }
@@ -78,6 +152,14 @@ const greedyAlgo = (items, capacity) => {
 
   return result;
 };
+
+/* 
+=====  Bottom Up Iterative =====
+*/
+
+//   function bottomUp(items, capacity) {
+//     let cache = Array(items.length);
+//   }
 
 const argv = process.argv.slice(2);
 
@@ -107,3 +189,7 @@ for (let l of lines) {
     value: value
   });
 }
+
+console.log(naiveKnapsack(items, capacity));
+console.log(memoizedKnapsack(items, capacity));
+console.log(greedyAlgo(items, capacity));
